bb8942609dd5f0228862e800815bbe3a890739b2
Alex Luong <aluong@crimson.ua.edu>
2019-09-07 17:59:24 -0500

    chore: clean up repo for v2

--- gatsby-node.js
+++
@@ -1,8 +0,0 @@
-exports.onCreatePage = async ({ page, actions }) => {
-  const { createPage } = actions
-
-  if (page.path.match(/^\/thread/)) {
-    page.matchPath = "/thread/*"
-    createPage(page)
-  }
-}
---
+++ server/functions/graphql.js
@@ -0,0 +1,23 @@
+const { ApolloServer, gql } = require("apollo-server-lambda")
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: (root, args, context) => {
+      return "Hello, world!"
+    },
+  },
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true,
+})
+
+export const handler = server.createHandler()
