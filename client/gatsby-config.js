9f7fed023f83e7e67da6b60c37301e1d0863f475
Alex Luong <aluong@crimson.ua.edu>
2019-09-24 02:54:11 -0500

    fix: react helmet

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -3,6 +3,7 @@ require("dotenv").config()
 module.exports = {
   plugins: [
     "gatsby-plugin-emotion",
+    "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
